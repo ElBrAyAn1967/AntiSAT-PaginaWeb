27037a0a027b5cc602870e973b901c17dec40a97
ElBrayan.eth <99924775+ElBrAyAn1967@users.noreply.github.com>
2024-07-05 20:14:29 -0600

    colores favoritos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,17 @@ import tlwtypography from '@tailwindcss/typography'
 import tlwaspectratio from '@tailwindcss/aspect-ratio'
 
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+		Popato: '#ff9900',
+			Comino: '#abcdef',
+		  },
 		extend: {},
 	},
+
+
 	plugins: [animations, tlwforms, tlwtypography, tlwaspectratio],
 }
